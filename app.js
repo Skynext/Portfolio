3e494d76308ee39e9eedaea1135925cd8262da4b
Benjamin Colle <benjicolle@gmail.com>
2020-10-12 00:33:06 -0400

    save

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
 // particles background
-particlesJS.load('particles-js', 'assets/particles.json', function () {
+particlesJS.load('particles-js', 'assets/particlesjs-config.json', function () {
   console.log('callback - particles.js config loaded');
 });
 
